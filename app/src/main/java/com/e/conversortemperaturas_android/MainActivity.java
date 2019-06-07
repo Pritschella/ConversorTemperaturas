b28a64513d723358190d6b041bc717513d3f461d
Pritschella Flores <pritsflores@gmail.com>
2019-06-07 02:40:09 -0500

    Commit "Se agregó código"

--- app/src/main/java/com/e/conversortemperaturas_android/MainActivity.java
+++ app/src/main/java/com/e/conversortemperaturas_android/MainActivity.java
@@ -17,12 +17,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnCli
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);   
         setContentView(R.layout.activity_main);
         cajaNumAConvertir=findViewById(R.id.editText_convertir);
         cajaC=findViewById(R.id.editText_C);
         cajaF=findViewById(R.id.editText_F);
-        cajaR=findViewById(R.id.editText_R); 
+        cajaR=findViewById(R.id.editText_R);
         cajaK=findViewById(R.id.editText_K);
 
         radioC=findViewById(R.id.radioButtonC);
